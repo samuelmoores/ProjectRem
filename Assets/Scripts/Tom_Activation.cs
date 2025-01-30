6a7d54cc132c6c9c26f4d1c8fb21b54628d45e0f
Sam Moores <samueldmoores@gmail.com>
2025-01-29 19:39:46 -0800

    tree

--- Assets/Scripts/Tom_Activation.cs
+++ Assets/Scripts/Tom_Activation.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class Tom_Activation : MonoBehaviour
 {
     public GameObject Tom;
+    public GameObject BedRoomLight;
+    
     NavMeshAgent agent;
     GameObject Todd;
 
@@ -14,6 +17,19 @@ public class Tom_Activation : MonoBehaviour
     {
         agent = Tom.GetComponent<NavMeshAgent>();
         Todd = GameObject.Find("Todd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance_to_bed = Vector3.Distance(agent.pathEndPosition, Tom.transform.position);
+
+        if(agent.hasPath && distance_to_bed < 2.0f)
+        {
+            BedRoomLight.GetComponent<Light>().intensity += Time.deltaTime * 3.0f;
+            BedRoomLight.GetComponent<Light>().range += Time.deltaTime * 0.00f;
+
+        }
     }
 
     private void OnTriggerEnter(Collider other)
